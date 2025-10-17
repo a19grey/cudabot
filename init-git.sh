60462906b7dff3170a8f5ec538c9ebbd23cd2296
Alex Krause <a19grey@gmail.com>
2025-10-17 06:16:32 +0000

    initial commit into new public repo functional cuda chat v1

--- init-git.sh
+++ init-git.sh
@@ -6,6 +6,3 @@ git init
 git config user.name  "$GIT_USER_NAME"
 git config user.email "$GIT_USER_EMAIL"
 git remote add origin "$GITHUB_REMOTE"
-git add .
-git commit -m "Initial commit"
-git push -u origin main
